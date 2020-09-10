5298b8ff8833f191ff734af39094eb84e5439c5d
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-09-10 15:49:58 +0200

    importing functions from tastsSlice to Buttons

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,27 +1,34 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Container, Button } from "./styled";
+import { selectTasks, toggleHideDone, setAllDone } from "../tasksSlice";
 
-const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
-    <Container>
-        {tasks.length > 0 && (
-            <>
-                <Button
-                    onClick={toggleHideDone}
-                    hidden={tasks.length === 0}
-                >
-                    {hideDone ? "Pokaż ukończone" : "Ukryj ukończone"}
-                </Button>
-                <Button
-                    onClick={setAllDone}
-                    disabled={tasks.every(({ done }) => done)}
-                    hidden={tasks.length === 0}
-                >
-                    Ukończ wszystkie
-                </Button>
-            </>
-        )}
-    </Container>
+const Buttons = () => {
+    const { tasks, hideDone } = useSelector(selectTasks);
+    const dispatch = useDispatch();
+    
+    return (
+        <Container>
+            {tasks.length > 0 && (
+                <>
+                    <Button
+                        onClick={() => dispatch(toggleHideDone())}
+                        hidden={tasks.length === 0}
+                    >
+                        {hideDone ? "Pokaż ukończone" : "Ukryj ukończone"}
+                    </Button>
+                    <Button
+                        onClick={() => dispatch(setAllDone())}
+                        disabled={tasks.every(({ done }) => done)}
+                        hidden={tasks.length === 0}
+                    >
+                        Ukończ wszystkie
+                    </Button>
+                </>
+            )}
+        </Container>
 
-);
+    )
+};
 
 export default Buttons;
