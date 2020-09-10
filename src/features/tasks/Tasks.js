fc5fd66a9fd0023fdb5401e74ac0c67e7f1d6cbf
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-09-10 15:52:02 +0200

    removing export functions to components

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,47 +1,30 @@
-import React, { useState } from 'react';
-import useTasks from "../../useTasks";
+import React from 'react';
+import { useSelector } from "react-redux";
 import Form from "./Form";
 import TaskList from "./TaskList";
 import Buttons from "./Buttons";
 import Section from "../../common/Section";
 import Header from "../../common/Header";
 import Container from "../../common/Container";
-
+import { selectTasks } from './tasksSlice';
 
 function Tasks() {
-  const {tasks, removeTask, toggleTaskDone, setAllDone, addNewTask} = useTasks();
-
-  const [hideDone, setHideDone] = useState(false);
-  
-  const toggleHideDone = () => {
-    setHideDone(hideDone => !hideDone);
-  };
+  const { tasks } = useSelector(selectTasks);
 
   return (
     <Container>
       <Header title="Lista zadań" />
       <Section
         title="Dodaj nowe zadanie"
-        body={<Form addNewTask={addNewTask} />}
+        body={<Form />}
       />
-
       <Section
         title="Lista zadań"
         body={
-          <TaskList
-            tasks={tasks}
-            hideDone={hideDone}
-            removeTask={removeTask}
-            toggleTaskDone={toggleTaskDone}
-          />
+          <TaskList />
         }
         extraHeaderContent={
-          <Buttons
-            tasks={tasks}
-            hideDone={hideDone}
-            toggleHideDone={toggleHideDone}
-            setAllDone={setAllDone}
-          />
+          <Buttons />
         }
       />
     </Container>
