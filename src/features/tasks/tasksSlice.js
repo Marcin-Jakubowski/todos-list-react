f089d5894ba1d302eaffb284278f62d04dba80a4
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-09-10 15:47:51 +0200

    adding functions to tasksSlice

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -4,14 +4,31 @@ const tasksSlice = createSlice({
     name: `tasks`,
     initialState: {
         tasks: [],
+        hideDone: false,
     },
     reducers: {
         addTask: ({ tasks }, { payload }) => {
             tasks.push(payload);
         },
+        toggleHideDone: state => {
+            state.hideDone = !state.hideDone;
+        },
+        toggleTaskDone: (state, action) => {
+            const index = state.tasks.findIndex(task => task.id === action.payload);
+            state.tasks[index].done = !state.tasks[index].done;
+        },
+        removeTask: (state, action) => {
+            const index = state.tasks.findIndex(task => task.id === action.payload);
+            state.tasks.splice(index, 1);
+        },
+        setAllDone: state => {
+            for (state.task of state.tasks) {
+                state.task.done = true;
+            }
+        },
     },
 });
 
-export const { addTask } = tasksSlice.actions;
+export const { addTask, toggleHideDone, toggleTaskDone, removeTask, setAllDone } = tasksSlice.actions;
 export const selectTasks = state => state.tasks;
 export default tasksSlice.reducer;
