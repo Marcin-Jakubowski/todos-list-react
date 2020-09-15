60f72287789c55d32035fff71e88e48c19b17cce
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-09-16 01:37:25 +0200

    adding react router and navigation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,35 @@
 import React from "react";
-import Tasks from "./features/tasks/Tasks";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import TasksPage from "./features/tasks/TasksPage/index";
+import TaskPage from "./features/tasks/TaskPage/index";
+import AuthorPage from "./features/author/AuthorPage";
+import { StyledNavLink, NavList, NavListItem } from "./styled";
 
-export default () => <Tasks />;
+export default () => (
+    <HashRouter>
+        <nav>
+            <NavList>
+                <NavListItem>
+                    <StyledNavLink to="/zadania">Zadania</StyledNavLink>
+                </NavListItem>
+                <NavListItem>
+                    <StyledNavLink to="/autor">O autorze</StyledNavLink>
+                </NavListItem>
+            </NavList>
+            <Switch>
+                <Route path="/zadania/:id">
+                    <TaskPage />
+                </Route>
+                <Route path="/zadania">
+                    <TasksPage />
+                </Route>
+                <Route path="/autor">
+                    <AuthorPage />
+                </Route>
+                <Route path="/">
+                    <Redirect to="/zadania" />
+                </Route>
+            </Switch>
+        </nav>
+    </HashRouter>
+);
